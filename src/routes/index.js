fd3add60755080804a1d1daa23f96ade893cb40e
Danilo <danilojssantos1@gmail.com>
2024-03-26 16:24:33 -0300

    create notes

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,10 @@
 
 const {Router} = require("express")
 const  usersRoutes = require("./user.routes")
+const notesRoutes = require("./notes.routes")
 
 const routes = Router();
 routes.use("/users", usersRoutes)
+routes.use("/notes", notesRoutes)
 
 module.exports = routes;
